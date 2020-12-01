73b86a875f95f5718f8a5f59dab5417f69538061
Nice3point <nice3point@gmail.com>
2020-12-01 14:25:51 +0300

    Init project

---
+++ ADSK-Automatization/CopyADSK.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Plugin
+{
+    public class CopyAdsk : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
